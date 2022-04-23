1aeb3bad6f355c78a992e97bae558814edfdab4c
GarFunkle <vincent.durret@gmail.com>
2022-04-23 22:05:22 +0200

    first commit

---
+++ animatesprite/animatesprite.py
@@ -0,0 +1,104 @@
+import pygame
+from animatesprite.Support_Animation.support import import_folder
+
+
+class Player_Sprite(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, speed_walk=3, speed_run=5):
+        super().__init__()
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.25
+        self.image = self.animations['idle'][self.frame_index]
+        self.rect = self.image.get_rect()
+
+        self.status = 'idle'
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
+        self.sprite = pygame.image.load('assets/PLAYER/idle/idle.png')
+        self.image.set_colorkey([0, 0, 0])
+        self.rect = self.image.get_rect()
+        self.position = [x, y]
+        self.speed_walk = speed_walk
+        self.speed_run = speed_run
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+
+    def save_location(self):
+        self.old_position = self.position.copy()
+
+    def move_right(self):
+        self.facing_right = True
+        self.position[0] += self.speed_walk
+        self.status = 'run'
+        self.animation_speed = 0.35
+
+    def move_left(self):
+        self.facing_right = False
+        self.position[0] -= self.speed_walk
+        self.status = 'run'
+        self.animation_speed = 0.35
+
+    def update(self):
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def move_back(self):
+        self.position = self.old_position
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def get_image(self, x, y):
+        image = pygame.Surface([138, 138])
+        image.blit(self.sprite, (0, 0), (x, y, 138, 138))
+        return image
+
+    def import_character_assets(self):
+        character_path = 'assets/PLAYER/'
+        self.animations = {'idle': [], 'run': [],
+                           'jump': [], 'attack': [], 'dead': []}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations[self.status]
+
+        # loop over frame index
+        self.frame_index += self.animation_speed
+
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        image = animation[int(self.frame_index)]
+
+        if self.facing_right:
+            self.image = image
+        else:
+            self.facing_right = False
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
+        # set the rect
+        if self.on_ground and self.on_right:
+            self.rect = self.image.get_rect(
+                bottomright=self.rect.bottomright)
+        elif self.on_ground and self.on_left:
+            self.rect = self.image.get_rect(bottomleft=self.rect.bottomleft)
+        elif self.on_ground:
+            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+        elif self.on_ceiling and self.on_right:
+            self.rect = self.image.get_rect(topright=self.rect.topright)
+        elif self.on_ceiling and self.on_left:
+            self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop=self.rect.midtop)
+
+    def getstatus(self):
+        self.status
+
+    def get_rect(self):
+        return self.rect
---
+++ game.py
@@ -0,0 +1,98 @@
+import pygame
+import pytmx
+import pyscroll
+from animatesprite.animatesprite import Player_Sprite
+
+
+class Game:
+
+    def __init__(self):
+        # creation de la fenetre du jeu
+        self.screen = pygame.display.set_mode((1260, 700))
+        pygame.display.set_caption("Pygame test")
+
+        # charger la carte
+        tmx_data = pytmx.util_pygame.load_pygame('tile/carte.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(
+            map_data, self.screen.get_size())
+
+        # generer un joueur
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = Player_Sprite(player_position.x, player_position.y)
+        self.sol = tmx_data.get_object_by_name("sol")
+        # definir une liste qui va stocker mec collision
+        self.ground = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.ground.append(pygame.Rect(
+                    obj.x, obj.y, obj.width, obj.height))
+
+        self.gravity = (0, 10)
+        self.resistance = (0, 0)
+        self.collision_sol = False
+
+        # dessiner ke groupe de calques
+        self.group = pyscroll.PyscrollGroup(
+            map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
+
+        self.pressed = {}
+
+    def move(self):
+        if self.pressed.get(pygame.K_RIGHT):
+            self.player.move_right()
+        if self.pressed.get(pygame.K_LEFT):
+            self.player.move_left()
+
+    def gravity_game(self):
+        self.player.rect.y += self.gravity[1] + self.resistance[1]
+
+    def update(self):
+        self.group.update()
+
+        # for sprite in self.group.sprites():
+        #     if sprite.feet.collidelist(self.ground) > -1:
+        #         sprite.move_back()
+# boucle du jeu
+    def run(self):
+        clock = pygame.time.Clock()
+
+        running = True
+
+        while running:
+
+            self.player.save_location()
+            self.move()
+            self.update()
+            self.group.center(self.player.rect)
+            self.group.draw(self.screen)
+            self.player.animate()
+
+            self.gravity_game()
+
+            if self.sol:
+                self.resistance = (0, -10)
+                self.collision_sol = True
+
+            else:
+                self.resistance = (0, 0)
+
+            # if self.player.to_jump and self.collision_sol:
+            #     self.player.move_jump()
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+
+                if event.type == pygame.KEYDOWN:
+                    self.pressed[event.key] = True
+
+                elif event.type == pygame.KEYUP:
+                    self.player.status = 'idle'
+                    self.pressed[event.key] = False
+
+            clock.tick(60)
